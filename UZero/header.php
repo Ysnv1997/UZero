5d7d63cae9e08e0e551fdd0a4a756f0ee3cbf4fa
Ysnv <100954636@qq.com>
2019-08-30 17:57:37 +0800

    1

--- UZero/header.php
+++ UZero/header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title><?php wp_title( ' - ', true, 'right' ); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="http://at.alicdn.com/t/font_1372667_f0yzpjpgc4d.css">
 
